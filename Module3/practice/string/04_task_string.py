701f96792c4cd911fb74aa5b1eae65e1b599dd82
Sergey Martynov <64045794+Maniac-tv@users.noreply.github.com>
2020-12-06 14:08:41 +0300

    Update 04_task_string.py
    На проверку

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -2,6 +2,12 @@
 # Определить в предоставленном сообщении количество слов длиной больше, чем 5.
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
-# Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
-# TODO: your code here
+text = text.split()
+i = len(text)
+c = 0;
+n=0;
+while n<i-1:
+    if(len(text[n])>5):
+        c += 1
+    n=n+1
+print(c)
