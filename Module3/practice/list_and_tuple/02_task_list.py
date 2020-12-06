4753ac594aa87dcc252984afab5f16c8c4082328
KatyaS <katya@erofeeva.me>
2020-12-06 15:13:37 +0300

    Update 02_task_list.py

--- Module3/practice/list_and_tuple/02_task_list.py
+++ Module3/practice/list_and_tuple/02_task_list.py
@@ -2,5 +2,6 @@
 # Каждый элемент должен быть выведен с новой строки.
 
 fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
-
+for i, el in enumerate(fruits):
+    print(i + 1, el)
 # TODO: your code here
