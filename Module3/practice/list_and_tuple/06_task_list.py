85d6a98fe26c31ffef63f1e1038e9444e1afd275
Rodion4ik <rodion.vlasov@mail.ru>
2020-12-06 16:06:05 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,18 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input())
+second_number = int(input())
+
+if first_number > second_number:
+    less = second_number
+    more = first_number
+else:
+    more = second_number
+    less = first_number
+
+i = less
+while i <= more:
+    if i % 3 == 0:
+        print(i)
+    i +=1;
