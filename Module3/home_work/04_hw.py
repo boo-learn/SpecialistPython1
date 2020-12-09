3b58304f281a6ff4ac223fcca62dc7ce8e019d9c
Konchev <75563897+Konchev@users.noreply.github.com>
2020-12-09 23:12:09 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,21 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+numbers = [2, -5, 8, 9, -25, 25, 4]
+
+# import random
+# numbers = []
+# for i in range(random.randint(8, 10)):
+#     numbers.append(random.randint(4, 81))
+# print(numbers)
+
+new_numbers = []
+
+i = 0
+for number in numbers:
+    if number > 0 and number ** (1/2) % 1 == 0:
+        new_numbers.append(int(number ** (1/2)))
+        i += 1
+
+print(new_numbers)
