2a0c501294a893d23ccc4af5da4a9b209e41bb36
KatyaErofeeva <katya@erofeeva.me>
2020-12-13 14:39:06 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,15 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def split60(n):
+    return (n % 60, n // 60)
+
+
+def format_time(seconds):
+    sec, minutes = split60(seconds)
+    minutes, hours = split60(minutes)
+
+    return "{}:{}:{}".format(hours, minutes, sec)
+
+
+print(format_time(456356))
