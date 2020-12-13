e94ababa8a9e7b3bb26324e34ac298648877491e
Rodion4ik <rodion.vlasov@mail.ru>
2020-12-13 15:24:19 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -9,3 +9,16 @@ def average(*args):
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
 print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+
+
+def average(*args):
+    count = 0
+    all_sum = 0
+    for el in args:
+        all_sum += el
+        count +=1
+    return  all_sum/count
+
+print(average(3, 4, 8))
+print(average(1, 4, 5, -3, 8, 4))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2))
