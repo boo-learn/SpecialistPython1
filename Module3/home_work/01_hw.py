643db61d796fc01fb34d6d3a0208f88891b9613a
ChekLub <76036839+ChekLub@users.noreply.github.com>
2020-12-15 22:11:54 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -7,3 +7,28 @@ names = ["Иван", "Ирина", "Вячеслав", "Василий", "Пет
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
+
+if 0:
+    str_name = ''
+    for name in names:
+        str_name += name + ','
+    print(str_name[:len(str_name)-1])
+
+#============================
+if 1:
+    lng_names = len(names)
+    str_name = ''
+    num = 0
+    while num < lng_names-1:
+        str_name += names[num] + ','
+        num += 1
+    str_name += names[num]
+
+    print(str_name)
+#============================
+if 0:
+    sjoin = ','.join(names)
+    print(sjoin)
+
+# Пример вывода:
+# Иван, Ирина, Вячеслав, Василий, Петр
