c37d36d9ab99438036dc6e22ecbc51414d7b4b76
Konchev <75563897+Konchev@users.noreply.github.com>
2020-12-15 22:02:10 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -4,10 +4,24 @@
 
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
+import math
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+def triangle(p1, p2, p3):
+    ab = distance(*p1, *p2)
+    bc = distance(*p2, *p3)
+    ac = distance(*p1, *p3)
+    if ab - bc < 0 and ab - ac < 0:
+        #return ab
+        return "AB"
+    if bc - ac < 0 and bc - ab < 0:
+        #return bc
+        return "BC"
+    #return ac
+    return "AC"
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+p1, p2, p3 = ((10, 15), (12, 15), (13, 15))
+#print(triangle(p1, p2, p3))
+
+print("Самый короткий отрезок:", triangle(p1, p2, p3))
