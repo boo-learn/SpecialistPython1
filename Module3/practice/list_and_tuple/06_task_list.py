61bf1d995e8c21d6bc320838ab38b464767bc8c6
Firsoffdv <76036678+Firsoffdv@users.noreply.github.com>
2020-12-15 14:33:32 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,11 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+lst = list()
+i = min(first_number,second_number)
+while i <= max(first_number,second_number):
+    if i % 3 == 0:
+        lst.append(i)
+    i += 1
+
+print(lst)
