7cfb347410458c7e399c0e1c2ecb892f743e2b62
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-15 22:41:35 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -2,8 +2,21 @@
 # Выведите все имена в одну строку через запятую
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-
-# TODO: your code here
-
+if 0:  # ВАРИАНТ 1. Работает
+    str_to_print = ''
+    for name in names:
+        str_to_print += name
+        str_to_print += ', '
+    print(str_to_print[:-2])
+if 0:  # ВАРИАНТ 2. этот вариант усекает список в цикле
+    str_to_print = ''
+    names.reverse()
+    while names:
+        str_to_print += names.pop()
+        str_to_print += ', '
+    print(str_to_print[:-2])
+if 1:  # ВАРИАНТ 3. использует str.join
+    delim = ', '
+    print(delim.join(names))
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
