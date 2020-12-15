8139f8a37586d62cba9001b7e6583b6a1fa235cf
ChekLub <76036839+ChekLub@users.noreply.github.com>
2020-12-15 23:10:08 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,22 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+numbers = [2, -5, 8, 9, -25, 25, 4]
+
+if 0:
+    sqrt_numbers = []
+    for num in numbers:
+        sqrt_num = num**0.5
+        if num > 0 and (round(sqrt_num) - sqrt_num) == 0:
+            sqrt_numbers.append(sqrt_num) 
+    print(sqrt_numbers)
+#======================
+
+if 1:
+    sqrt_numbers = []
+    for num in numbers:
+        sqrt_num = num**0.5
+        if num > 0 and sqrt_num.is_integer():
+            sqrt_numbers.append(sqrt_num) 
+    print(sqrt_numbers)
