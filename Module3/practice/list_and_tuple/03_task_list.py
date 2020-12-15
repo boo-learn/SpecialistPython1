4af7cb1abeb8540d84fcd6eedde429fc37a76197
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2020-12-15 13:03:34 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -1,4 +1,9 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех элементов.
+numb_list=[-10,8,7,-5,7,-3,6,-1,9,3]
+summa_numb=0
+for ind in numb_list:
+    summa_numb+=ind
+print(summa_numb)
 
 # TODO: your code here
