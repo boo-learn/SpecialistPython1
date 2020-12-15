a8e2f43bb7ec5174712f466fd7a80cd85589e7da
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-15 22:46:31 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,20 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import random
+
+element_count = 4  # n
+
+numbers = list()
+for _ in range(0, element_count):
+    numbers.append(random.randint(8, 16))
+print('dano: ', numbers)
+
+new_list = list()
+for el in numbers:
+    if el <= 0:
+        continue
+    boolean = int(el ** 0.5) == el ** 0.5
+    if boolean:
+        new_list.append(el)
+print('result: ', new_list)
