dec343907de8b3c6332a4dc39575ab425b64d8cb
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-15 12:00:12 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,9 @@
 # Подсчитать количество букв «а» во введенной строке
-
-# TODO: your code here
+string = input('Input your string: ')
+count_a = string.count('a')
+print('число букв а', count_a)
+# Интересное дело, литера а бывает кириллической
+str1 = 'напомни маме про раму'
+count_a_cyr = str1.count('а')
+count_a = str1.count('a')
+print('count_a_cyr', count_a_cyr, '!= count_a', count_a)
