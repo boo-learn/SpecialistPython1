7ebfc1d93fd40778c1f6b64b80178fd62b5ff363
ChekLub <76036839+ChekLub@users.noreply.github.com>
2020-12-15 12:11:04 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,15 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+splt = text.split(' ')
+num_words = len(splt)
+
+print(splt)
+
+i = 0
+res = 0
+while i<num_words:
+    if len(splt[i]) > 5:
+        res +=1
+    i += 1
+print(res, 'слов длиной более 5 букв')
