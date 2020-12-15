8e02a934f9d18221c31c64528b6699c433779205
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-15 12:30:12 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,17 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
+# Without using lists
 
-# TODO: your code here
+# TODO: here we should trim trailing spaces
+word_begin = 0
+word_end = 0
+word_end = text.find(' ', word_begin)
+counter =  0
+while word_end < len(text):
+    word_end = text.find(' ', word_begin)
+    word_length = word_end - word_begin
+    #test_word = text[word_begin:word_end]    #for debug
+    word_begin = word_end + 1
+    if word_length > 5:
+        counter +=1
