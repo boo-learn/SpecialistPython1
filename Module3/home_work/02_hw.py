d3f57789570e8e320af0a0a0b2a80bae69726f62
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-15 22:42:32 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -3,6 +3,13 @@
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
 import random
-numbers = []
+
+# numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
-# TODO: your code here
+
+element_count = 12  # n
+if 1:  # VAR1. Идея в том, чтобы инициировать пустой список, а затем использовать  for ... in range
+    numbers = list()
+    for _ in range(0, element_count):
+        numbers.append(random.randint(-100, 100))
+print(numbers)
