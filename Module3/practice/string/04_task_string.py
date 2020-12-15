0f0de52ad2a50acfcb7ba7d71c43b92fcdbf7a2a
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2020-12-15 12:07:38 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,13 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
+list=text.split(" ")
+i=0
+count=0
+while i<len(list):
+    if len(list[i])>5:
+        count+=1
+    i+=1
+print(text)
+print("Количество слов больше 5 символов в тексте равно: ",count)
 # TODO: your code here
