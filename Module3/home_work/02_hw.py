9ce1159a96e4a902fe4646a9c3250e5d1dff3f1d
Firsoffdv <76036678+Firsoffdv@users.noreply.github.com>
2020-12-15 21:55:49 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -5,4 +5,9 @@
 import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
-# TODO: your code here
+n = int(input('Введите кол-во элементов: '))
+while n > 0:
+    numbers.append(random.randint(-100, 100))
+    n -= 1
+
+print(numbers)
