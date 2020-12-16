96a2b78873486b456658aacc5f8edd173a086238
Firsoffdv <76036678+Firsoffdv@users.noreply.github.com>
2020-12-16 23:58:56 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -5,9 +5,17 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return ((x2-x1)**2+(y2-y1)**2)**(1/2)
 
+def min_segment (a, b, c):
+   segments = list(zip(('AB','BC','AC'), (distance(*(a+b)), distance(*(b+c)),distance(*(a+c)))))
+   min_lenght = min(segments[0][1], segments[1][1], segments[2][1])
+   for seg in segments:
+       if seg[1] == min_lenght:
+           return seg[0]
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+a = (-20, 12)
+b = (14, 18)
+c = (12, -12)
+
+print("Самый короткий отрезок: ",min_segment (a, b, c))   # Выводим название отрезка, например “АС”.
