1b55616282411bfe82a9ac211bc87b97895f000d
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2020-12-16 13:11:59 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,19 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+ef fibonacci_n(n):
+    first=0
+    sekond=1
+    i=2
+    while i<n:
+        i+=1
+        next=first+sekond
+        first=sekond
+        sekond=next
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return next
+
+n=7
+print(fibonacci_n(n))
