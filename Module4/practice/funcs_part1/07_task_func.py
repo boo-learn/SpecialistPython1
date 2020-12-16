43338492393c0fecf6f37db9bc912fc77d6b7080
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2020-12-16 12:55:45 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,16 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def format_time(second):
+    ss=second%60
+    second=second//60
+    mm = second % 60
+    second = second // 60
+    hh=second
+    list_time=hh,mm,ss
+    return list_time
+    #print(list_time)
+
+sec=29143
+
+print(f"{format_time(sec)[0]:0>2}:{format_time(sec)[1]:0>2}:{format_time(sec)[2]:0>2}")
