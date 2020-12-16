9e38abb684b2f51f402aded79c66cfbea4500194
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-16 11:34:06 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,10 +4,19 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
+    number_of_digits = 0
+    while number > 10 ** number_of_digits:
+        number_of_digits += 1
+
+    # print  (10 ** number_of_digits - 1)
+    first_digit = number // 10 ** (number_of_digits - 1)
+    last_digit = number % 10
+    other_didits = (number - first_digit * 10 ** (number_of_digits - 1)) // 10
+    return (first_digit == last_digit) or (palindrome(other_didits) and (other_didits != 0))
+
+    # Тестируем функцию
 
 
-# Тестируем функцию
 print(palindrome(3454))
 print(palindrome(3443))
 print(palindrome(1234541))
