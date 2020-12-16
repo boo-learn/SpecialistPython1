73337101662a3e4e76a5dd7e23a423c862e0170e
ChekLub <76036839+ChekLub@users.noreply.github.com>
2020-12-16 11:33:38 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -2,11 +2,28 @@
 # Палиндром - число читающееся одинаково слева направо и справа налево.
 # Пример палиндрома: 34543
 # * попробуйте решить данную задачу, не преобразуя число к строке
-
 def palindrome(number):
-    pass
+    z = 0
+    num = number
+    while num // 10 > 0:
+        num = num // 10
+        z += 1
 
+    buf = 0
+    number_ = number
+    for i in range(z):
+        if number % 10 == number_ // 10**(z-i):
+            buf += 1
+            number = number // 10
+            a = number_ // 10**(z-i)
+            number_ = number_ - a*10**(z-i)
+        else:
+            buf = 0
 
+    if buf > 0:
+        return ('YES')
+    else:
+        return ('NO')
 # Тестируем функцию
 print(palindrome(3454))
 print(palindrome(3443))
