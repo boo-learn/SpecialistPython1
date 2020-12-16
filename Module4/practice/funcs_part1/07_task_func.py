b7b2583acc489729a4433d1f63c85a06381d9ed8
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-16 12:43:42 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,9 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def string_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    sec = seconds % 3600 % 60
+    return f"{hours}:{minutes}:{sec}"
+print(string_time(29143))
