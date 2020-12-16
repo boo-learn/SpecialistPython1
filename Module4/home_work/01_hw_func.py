b976a93df06d4d1cd8a2e93ebea70be10de84a92
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-16 21:40:06 +0300

    Update 01_hw_func.py
    В продолжение темы с генераторами попробовал реализовать разбор числа в последовательность цифр. Удивительно, но получилось! Интересно, что быстрее?
    Есть подозрение, что операции с числами чуть шустрее, но это неточно.
    Да и конструкция выглядит искусственной.  Вы когда-нибудь что-то подобное видели/делали?

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -1,13 +1,36 @@
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
+def num_to_digits_sequence(num):
+    '''
+    Генератор преобразует число в последовательность цифр, начиная с последней цифры и двигаясь к началу
+    1234 --> 4, 3, 2, 1
+    :param num: Целое число
+    :return:
+    '''
+    while num:
+        digit = int(num % 10)
+        num //= 10
+        yield digit
+
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    # Решение со строками
+    if 0:
+        pass
+        string = str(ticket_number)
+        digits = tuple(map(int, string))
+        return  sum(digits[:3]) == sum(digits[3:])
+
+    # экзотика с использованием генератора
+    if 1:
+        pass
+        digits = tuple(num_to_digits_sequence(ticket_number))
+        return sum(digits[:3]) == sum(digits[3:])
 
 
 # Тестируем функцию
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+print(lucky_ticket(123005))
+print(lucky_ticket(123321))
 print(lucky_ticket(436751))
