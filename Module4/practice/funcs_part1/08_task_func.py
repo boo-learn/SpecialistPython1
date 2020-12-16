abf2c633939853b44fea373a3fa501a9a5a08fa9
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-16 13:06:26 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,11 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fibonacci(n):
+    fib0 = 0
+    fib1 = 1
+    for _ in range(n):
+        fib0 = fib1
+        fib1 += fib0
+    return fib0
+
+for i in range(10):
+    print(fibonacci(i))
