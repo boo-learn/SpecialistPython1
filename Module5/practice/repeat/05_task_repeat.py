89f2f592e165e2b72d4b4f3094594256072f35c7
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2020-12-17 11:35:09 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,9 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination_tail(num_items, items_on_page):
+    count_tail=num_items%items_on_page
+
+    return count_tail
+
+print(pagination_tail(99,10))
