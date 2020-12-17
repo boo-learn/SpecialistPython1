fb0c63ec401c8a9629808d5d590410a4bb2b084b
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-17 15:16:05 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,12 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+import os
+import settings
+
+cass_bill_file = os.path.join(settings.BASE_DIR, 'files', 'sold.txt')
+with open(cass_bill_file) as f:
+    s = 0
+    for line in f:
+        s += sum(map(float, line.split()))
+print(s)
