d492d309d3e7dcf1b9a64a5bda3326504dc570c4
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-17 11:57:18 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,6 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
-
+num_items = 99
+item_on_page = 10
+items_on_last_page = num_items % item_on_page
+print(items_on_last_page, 'elements shown on the last page' )
