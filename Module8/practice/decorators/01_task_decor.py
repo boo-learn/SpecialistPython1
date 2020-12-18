ab93df010b570336c68b3399a0af3c71a2b1c284
NikitaPonosov <76036799+NikitaPonosov@users.noreply.github.com>
2020-12-18 16:03:23 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,26 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        string = str(func(*args, **kwargs))
+        print('*' * (4 + len(string)))
+        print('*', string, '*')
+        print('*' * (4 + len(string)))
+
+    return wrapper()
+
+
+@decor
+def return_true():
+    return True
+
+
+@decor
+def is_wrong_year(num_year):
+    return not (num_year % 4 != 0 or (num_year % 100 == 0 and num_year % 400 != 0))
+
+
+return_true()
+is_wrong_year(2000)
