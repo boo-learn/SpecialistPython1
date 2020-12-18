ef0b2b932620ba830a79dca8969d557703b187a9
ChekLub <76036839+ChekLub@users.noreply.github.com>
2020-12-18 16:03:40 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,32 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+def new_decorator(function_to_decorate):
+    def wrapper(*args):
+        # print("Я - код, который отработает до вызова функции")
+        # function_to_decorate()  # Сама функция
+
+        lst = list(function_to_decorate())
+        leng = len(lst)
+        out = []
+        for _ in lst:
+            out.append('*')
+
+        print(str(out))
+        print(function_to_decorate())
+        print(str(out))
+
+        # print("А я - код, срабатывающий после")
+
+    return wrapper
+
+
+# Вот так просто мы можем декорировать любую функцию
+@new_decorator
+def simple_function(text):
+    return text
+#
+#
+# # Вызываем декорируемую функцию:
+text = input('Введите слово:')
+simple_function(text)
