1c11922237678291ea6e1973b3792f9c915ea263
Firsoffdv <76036678+Firsoffdv@users.noreply.github.com>
2020-12-18 15:44:02 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,21 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+
+def stars(function_to_decorate):
+    def wrapper():
+        stroke = list(function_to_decorate().split(" "))
+        for el in stroke:
+            print('*'*len(str(el))+'**')
+            print(f'*{el}*')
+            print('*' * len(str(el)) + '**')
+        return
+
+    return wrapper
+
+@stars
+def re_line():
+    return input()
+
+
+re_line()
