be782d97ab81b63af7a2eb4d47e71af2cc5f7109
Andrey874 <60545859+Andrey874@users.noreply.github.com>
2020-12-19 17:52:35 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -5,9 +5,19 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+def size_tib(p1, p2, p3):
+  AB = distance(*p1, *p2)
+  AC = distance(*p1, *p3)
+  BC = distance(*p2, *p3)
+  if AB < AC and AB < BC:
+    return "Min Size AB:", AB
+  elif  AC < AB and AC < BC:
+    return "Min Size AC:", AC
+  else: return "Min Size AB:", BC
 
 
 # TODO: your code here
+print(size_tib((10, 5), (14, 22), (2, 12)))
 print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
