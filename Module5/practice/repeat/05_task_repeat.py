50709c1c93ea00d181ef86948b5b3bfae916df48
Sergey Martynov <64045794+Maniac-tv@users.noreply.github.com>
2020-12-20 11:17:29 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,7 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination(num_items, items_on_page):
+    if num_items%items_on_page!=0:
+        return num_items%items_on_page
+    return items_on_page
