63e31e286bca384ef195f17865d222174ad4abed
Konchev <75563897+Konchev@users.noreply.github.com>
2020-12-20 14:14:06 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,19 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+import os
+path = os.path.join('Files', 'data/sold.txt')
+sold = []
+
+with open(path, 'r') as f:
+    for line in f:
+        sold = sold + line.split()
+
+#print(sold)
+sold_f = []
+for el in sold:
+    sold_f.append(float(el))
+
+print(sum(sold_f))
+print(max(sold_f))
+print(min(sold_f))
