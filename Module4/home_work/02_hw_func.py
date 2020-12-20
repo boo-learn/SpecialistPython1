5789dbeed01a19b9e2bb9c71d42054096b49ce8a
Sergey Martynov <64045794+Maniac-tv@users.noreply.github.com>
2020-12-20 03:06:10 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -3,11 +3,17 @@
 # Если отрезков с минимальной длиной несколько - вывести любой
 
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
+import math
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
 
+def cmp_line(ab,bc,ca):
+    if ab<bc and ab<ca:
+        return('ab',ab)
+    elif bc<ab and bc<ca:
+        return ('BC', bc)
+    else:
+        return ('CA',ca)
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+print("Самый короткий отрезок:", cmp_line(distance(1,0,2,7), distance(2,7,7,0), distance(1,0,7,0)))
