838bc0b1a0278f1de1efe537731a549ba26378ce
Rodion4ik <rodion.vlasov@mail.ru>
2020-12-27 11:08:27 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,15 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+
+i = 0
+while i==0:
+ input_data = input("input  data in format 12x6")
+
+ a = input_data.split("x")
+
+ if(a[0].isdigit() and a[1].isdigit() and len(a)==2):
+  print(a);
+  i=1
+ else:
+    print("wrong")
