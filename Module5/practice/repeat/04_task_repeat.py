4be7645101cd112f92a45da5bb1cec0825760fd6
Dmitry Solovov <75861361+dsolovov@users.noreply.github.com>
2020-12-27 17:24:12 +0300

    Update 04_task_repeat.py

--- Module5/practice/repeat/04_task_repeat.py
+++ Module5/practice/repeat/04_task_repeat.py
@@ -1,10 +1,33 @@
-# Напишите функцию принимающую общее количество объектов(например, товаров)
-# которые нужно отобразить и количество объектов, которые нужно отобразить на каждой странице.
-# Функция должна вычислять количество страниц для отображения всех объектов.
+import json
+import pprint
+import urllib.request
+app_id = '90dcc46f'
+app_key = 'e16e576ca2c39bdbb28ccac2bdf6ce5a'
 
-# Под пагинацией понимают показ ограниченной части информации на одной
-# веб-странице и вывода списка остальных страниц.
 
-def pagination(num_items, items_on_page):
-    pass
+def get_data(url):
+    json_data = urllib.request.urlopen(url).read()
+    data = json.loads(json_data)
+    return data
 
+
+data = get_data("http://ip-api.com/json/")
+lat = data['lat']
+lon = data['lon']
+weather_data = get_data(f"http://api.weatherunlocked.com/api/current/{lat},{lon}?app_id={app_id}&app_key={app_key}")
+pprint.pprint(data)
+pprint.pprint(weather_data)
+
+city = data['city']
+wx_desc = weather_data['wx_desc']
+temp_c = weather_data['temp_c']
+windspd_kts = weather_data['windspd_kts']
+humid_pct = weather_data['humid_pct']
+str = f"""
+Погода сейчас в {city}
+{wx_desc}
+Температура: {temp_c} ℃
+Ветер: {windspd_kts} км/ч
+Влажность: {humid_pct} %
+"""
+print(str)
