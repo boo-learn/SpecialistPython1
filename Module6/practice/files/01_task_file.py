bbadbeb46f84ade85a49492b2b48654bd7737550
Sergey Martynov <64045794+Maniac-tv@users.noreply.github.com>
2020-12-27 11:05:08 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,15 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+t=False
+while(t!=True):
+    s=input('NxM')
+    try:
+        n=0
+        m=0
+        n=s[:s.find('x')]
+        m=s[s.find('x')+1:]
+        print(int(n)/int(m))
+        t=True
+    except ValueError:
+        print('Format error')
