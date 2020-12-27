f8a8c7ae48cca4cc3886d95c45d427226b2a3bd9
Sergey Martynov <64045794+Maniac-tv@users.noreply.github.com>
2020-12-27 11:05:45 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,15 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+t=False
+while(t!=True):
+    s=input('NxM')
+    try:
+        n=0
+        m=0
+        n=s[:s.find('x')]
+        m=s[s.find('x')+1:]
+        print(int(n)/int(m))
+        t=True
+    except ValueError:
+        print('Format error')
