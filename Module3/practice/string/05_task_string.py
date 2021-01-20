9864d6e1a92dc598b9046fd10d3eb1ece6ad8aae
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-20 20:28:06 +0300

    Update 05_task_string.py

--- Module3/practice/string/05_task_string.py
+++ Module3/practice/string/05_task_string.py
@@ -2,7 +2,21 @@
 # Ваша задача — определить, является ли эта строка «перевертышем», если удалить из нее все пробелы и знаки пуктуации.
 # Строка называется «перевертышем», если читается одинаково слева направо и справа налево.
 
-# Пример строки перевертыша: "И темен город. Мороз узором дорог не мети."
+str = 'И темен город Мороз узором дорог не мети'
 
-# TODO: your code here
+new_str = str.lower()
+print(new_str)
+words = new_str.split()
+
+i = 0
+converted_str = ''
+while i < len(words):
+    converted_str += words[i]
+    i += 1
+  
+converted_str_2 = converted_str[::-1]
+print(converted_str_2)
+if converted_str == converted_str_2:
+    print('Перевёртыш')
+
 
