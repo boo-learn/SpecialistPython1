ee0f4a55ec3bbaa0adcf39e40984b3184b0ecae0
AnatolyPushkov <77688127+AnatolyPushkov@users.noreply.github.com>
2021-01-20 13:09:44 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,11 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+
+a = int(input())
+if a % 5 == 0 and a % 3 == 0:
+    print("Foo")
+elif a % 5 == 0:
+    print("Bar")
+elif a % 3 == 0:
+    print("Foobar") 
