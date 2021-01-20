3f2ad7f3fd8f8dd08fead78ba25c9bd8e0ff304c
Dmitry.N <77688182+DrunkSpacer@users.noreply.github.com>
2021-01-20 20:05:24 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,11 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+word = input('Введите слово')
+x = 0
+y = 0
+while x < len(word):
+    if word[x] == 'а':
+        y += 1
+    x += 1
+print(y)
