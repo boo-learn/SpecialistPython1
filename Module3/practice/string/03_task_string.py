da2a51f31965c2c71e2e30ed9b9d38aacf5f2740
alexshat2897 <77632199+alexshat2897@users.noreply.github.com>
2021-01-20 20:14:09 +0300

    Update 03_task_string.py
    не работает, help

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,9 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+# Подсчитать количество букв «а» во введенной строке
+
+# TODO: your code here
+text = input('введи текст: ')
+count = text.count()
+print(count)
