b5418876fb2f7bf430906149f1e3d6840c25d45d
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-20 20:09:35 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,12 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
-# TODO: your code here
+words = text.split()
+i = 0
+count = 0;
+while i < len(words):
+    if len(words[i]) > 5:
+        count += 1
+        print(words[i])
+    i += 1
+print('Количество слов:', count)
