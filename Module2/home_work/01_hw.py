3de5960da48d09a01343de2a7fb5366155ba0fe5
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-20 16:02:54 +0300

    Update 01_hw.py
    Невнимательно прочитал условие. Показалось что во входных k < n * m

--- Module2/home_work/01_hw.py
+++ Module2/home_work/01_hw.py
@@ -4,5 +4,12 @@
 # Вводятся 3 натуральных числа n, m и k. Точно известно, что  k ≠ n ⋅ m.
 # Формат выходных данных
 # Выведите «YES», если можно отломить от шоколадки ровно k долек, и «NO» иначе.
+n = int(input('Введите n: '))
+m = int(input('Введите m: '))
+k = int(input('Введите k: '))
 
-# TODO: your code here
+
+if (not(k % m) or not(k % n)) and k < (n * m):
+    print('YES')
+else:
+    print('NO')
