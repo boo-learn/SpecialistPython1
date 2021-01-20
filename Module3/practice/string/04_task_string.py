5897360fd501e3a18a76ed8e3cfa14f0190115a1
Dmitry.N <77688182+DrunkSpacer@users.noreply.github.com>
2021-01-20 20:32:51 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,14 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+x = 0
+y = 0
+z = 0
+while x < len(text):
+    if text[x] == ' ':
+        y = 0
+    elif y == 6:
+        z += 1
+    y += 1
+    x += 1
+print(z)
