0e299ba38f9303f8a348f7e87e8e55b94ab55bdd
Artur-dev-sci <77688246+Artur-dev-sci@users.noreply.github.com>
2021-01-20 20:17:39 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,7 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+list = text.replace('.', '').split()
+for word in list:
+    if len(word) > 5:
+        print(word)
