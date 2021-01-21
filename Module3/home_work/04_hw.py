c6dc840d29ae67b3833f4c10a921fe0dceefd5ed
Artur-dev-sci <77688246+Artur-dev-sci@users.noreply.github.com>
2021-01-21 17:13:16 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+numbers = []
+list_sqrt = []
+
+n = int(input())
+
+for i in range(0, n):
+    numbers.append(random.randint(-100, 100))
+
+for number in numbers:
+    if number > 0 and (number**.5).is_integer():
+        list_sqrt.append(number)
+
+print(list_sqrt)
