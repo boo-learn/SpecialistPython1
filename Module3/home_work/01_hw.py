682200e224745ba6f1ff44bf2dd840dbf73abcaf
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-21 11:52:15 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -2,8 +2,9 @@
 # Выведите все имена в одну строку через запятую
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-
-# TODO: your code here
-
-# Пример вывода:
-# Иван, Ирина, Вячеслав, Василий, Петр
+names_in_string = ''
+for index, name in enumerate(names, 1):
+    if index < len(names):
+        name += ', '
+    names_in_string += name
+print(names_in_string)
