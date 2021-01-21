4b55c6feacda8d83f95366120da6f9b128654d7a
Artur-dev-sci <77688246+Artur-dev-sci@users.noreply.github.com>
2021-01-21 17:13:26 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -2,5 +2,13 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+len_name = 0
 
-# TODO: your code here
+for name in names:
+    if len_name < len(name):
+        len_name = len(name)
+
+for name in names:
+    if len_name == len(name):
+        print(name)
+        break
