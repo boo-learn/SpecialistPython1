c8dd9e3128690d675d42a55240c645fab0fcac2f
Peredvignik <77688198+Peredvignik@users.noreply.github.com>
2021-01-22 18:28:24 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -2,9 +2,23 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
+def revers_number(number): #
+    revers_num = 0
+    while number != 0:
+        revers_num += number % 10
+        number = number // 10
+        if number != 0:
+            revers_num *= 10
+    return revers_num
+#print(revers_number(12348768875678900005))
+
+def sum_two_last(number):
+    return number % 10 + number // 10 % 10
+
+
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+
+    return sum_two_last(ticket_number) == sum_two_last(revers_number(ticket_number))
 
 
 # Тестируем функцию
