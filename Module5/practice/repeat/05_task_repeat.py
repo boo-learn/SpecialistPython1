8ec475ca256cc90824cfd503a1c58370427cf864
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-22 21:20:21 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,10 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def last_page_items(num_items, items_on_page):
+    last_page = num_items % items_on_page
+    if last_page == 0:
+        return items_on_page
+    return last_page
+
+print(last_page_items(6, 3))
