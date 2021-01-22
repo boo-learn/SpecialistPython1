d417cddb74555c30e77bf207f4e8cb3fc7bbfdba
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-22 20:52:27 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,13 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    copy_num = number
+    reverse_num = 0
+    while number > 0:
+        reverse_num += number % 10
+        number //= 10
+        if number > 0:
+            reverse_num *= 10
+    return reverse_num == copy_num
+    
+print(palindrome(12321))
