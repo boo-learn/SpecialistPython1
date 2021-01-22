cacab2844362a0a19a596a9b44ef82bef2e48fac
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-22 15:34:18 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -1,13 +1,16 @@
+
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    ticket_list = list(str(ticket_number))
+    ticket_list = [int(i) for i in ticket_list]  # <- По хорошему тут надо обработку исключения
+    return len(ticket_list) == 6 and sum(ticket_list[:3]) == sum(ticket_list[3:])
 
 
 # Тестируем функцию
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+print(lucky_ticket(436752))
