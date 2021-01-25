a2b51303b1e4dd057fa9e98558036dc0e04d71d1
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-25 20:19:36 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,16 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+
+f = open('data\\solds.txt', 'r')
+
+price_list = []
+for line in f:
+    price_list_txt = line.split()
+    for price in price_list_txt:
+        price_list.append(float(price))
+
+print(price_list)
+print(sum(price_list))
+print(max(price_list))
+print(min(price_list))
