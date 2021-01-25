b68f180bdbf9286b948ade69de3f741b0505a401
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-25 19:59:18 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,23 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+
+in_file = open('data\\limerics.txt', 'r', encoding="utf-8")
+
+all_lines = []
+for line in in_file:
+   all_lines.append(line)
+   print(line)
+
+non_space = 0
+for line in all_lines:
+    non_space += len(line) - line.count(' ')
+
+print(non_space)
+
+poem_num = 1
+for line in all_lines:
+    if line[0] == '\n':
+        poem_num += 1
+
+print(poem_num)
