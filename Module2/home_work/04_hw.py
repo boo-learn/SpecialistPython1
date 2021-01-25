37c91e71c31fb9ffabb5bf27ec3a4aee196b2601
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-01-25 21:02:59 +0300

    Update 04_hw.py

--- Module2/home_work/04_hw.py
+++ Module2/home_work/04_hw.py
@@ -14,3 +14,22 @@
 #    #
 #    #
 ######
+square_side = int(input('enter square side value: '))
+
+line_counter = 1
+while line_counter <= square_side:
+    column_counter = 1
+    string = ''
+    if (line_counter == 1 or line_counter == square_side):
+        while column_counter <= square_side:
+            string += '#'
+            column_counter += 1
+    else:
+        while column_counter <= square_side:
+            if column_counter == 1 or column_counter == square_side:
+                string += '#'
+            else:
+                string += ' '
+            column_counter += 1
+    print(string)
+    line_counter += 1
