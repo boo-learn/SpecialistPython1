e7549cd49b906175042284811a8b8fc19e960510
AlexeyEgorov86 <77969661+AlexeyEgorov86@users.noreply.github.com>
2021-01-26 23:35:32 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -7,3 +7,10 @@ names = ["Иван", "Ирина", "Вячеслав", "Василий", "Пет
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
+
+my_string = ''
+for n_name in names:
+    my_string += n_name + ', '
+
+my_string = my_string[0:-2]
+print(my_string)
