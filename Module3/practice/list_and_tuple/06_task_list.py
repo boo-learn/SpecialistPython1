6ab5c2f40bee59677c5becb2ffd28cf3f36c8d20
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-01-26 13:02:00 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,20 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+list = []
+big = 0
+small = 0
+if first_number > second_number:
+    big = first_number
+    small = second_number
+else:
+    small = first_number
+    big = second_number
+
+while small <= big:
+    list.append(small)
+    small += 1
+
+for el in list:
+    if el%3 == 0:
+        print(el)
