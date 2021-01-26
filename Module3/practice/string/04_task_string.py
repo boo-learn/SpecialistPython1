c4dab2ec66494257b2e2d9118703820d2e7f9ce0
Twentysixth <77969614+Twentysixth@users.noreply.github.com>
2021-01-26 11:28:02 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,13 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+new_text = []
+new_text = text.split(' ')
+count = 0
+y = 0
+for i in new_text:
+    lenght = len(new_text[y])
+    y = y + 1
+    if lenght > 5:
+        count += 1
+print(count)
