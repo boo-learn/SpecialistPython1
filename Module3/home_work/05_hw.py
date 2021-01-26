630b139b09e2574f51d463e0ac35bcd7aba2781e
Twentysixth <77969614+Twentysixth@users.noreply.github.com>
2021-01-26 19:26:48 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,12 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+max_len = len(names[0])
+biggest_name = ''
+
+for el in names:
+    if len(el) > max_len:
+        max_len = len(el)
+        biggest_name = el
+print(biggest_name)
