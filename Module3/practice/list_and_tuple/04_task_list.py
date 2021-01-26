350bbb75ce2283795973ce0a3ba5a185e97657ea
Twentysixth <77969614+Twentysixth@users.noreply.github.com>
2021-01-26 12:19:41 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,11 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+my_list = [-1, 2, 5, -3]
+
+sum_positive_els = 0
+
+for el in my_list:
+    if el > 0:
+        sum_positive_els += el
+print(sum_positive_els)
