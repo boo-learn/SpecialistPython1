d351579ddf04406cae4405de933564a76bd5bfc1
Peredvignik <77688198+Peredvignik@users.noreply.github.com>
2021-01-26 14:43:09 +0300

    Update 02_hw_file.py
    проблема - не прибавляет -45

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,16 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+f_list = []
+digit_list = []
+sum_digits = 0
+with open("data/info.txt", "r", encoding="UTF-8") as f:
+    for i in f:
+        if i.strip().isdigit():
+            digit_list.append(int(i.strip()))
+            sum_digits += int(i.strip())
+        f_list.append(i.strip())
+
+print(f_list)
+print(digit_list)
+print(sum_digits)
