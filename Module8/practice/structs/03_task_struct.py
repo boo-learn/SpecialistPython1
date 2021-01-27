b79e020d861a61d2837b7ea282bff7b5be9af3d5
Dmitry.N <77688182+DrunkSpacer@users.noreply.github.com>
2021-01-27 20:25:23 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,20 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+import statistics
+
+def random_list(min, max, size):
+    list = []
+    for el in range(size):
+        new_el = random.randint(min, max)
+    return list.append(new_el)
+
+
+new_list = random_list(-10, 50, 10)
+
+not_more_10 = [el for el in new_list if el <= 10]
+
+positive_list = [el for el in new_list if el > 0]
+
+sum_list = statistics.mean(map(lambda el: el // 2 == 0, new_list))
