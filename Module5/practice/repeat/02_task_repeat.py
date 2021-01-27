5cb70398d54f08030f536b289347cf336cb3fc68
Andrej9173 <x9173@yandex.ru>
2021-01-27 23:34:09 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,16 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    temp_number = number
+    a = 0
+    i = 0
+    while number > 0:
+        a *= 10
+        i += 1
+        a += number % 10
+        number = number // 10
+    return temp_number == a
+
+
+ent_number = int(input('Введи число: '))
+print(palindrome(ent_number))
