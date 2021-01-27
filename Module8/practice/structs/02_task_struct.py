39ba68903e7caa9d8efdd3ac3ed9be28872767ed
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-27 19:50:44 +0300

    Update 02_task_struct.py

--- Module8/practice/structs/02_task_struct.py
+++ Module8/practice/structs/02_task_struct.py
@@ -5,3 +5,24 @@
 #   например, lst = [1, 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 2, 4, 5, 6]
 #   2. элементы исходного списка, которые не имеют повторений(встречаются только один раз):
 #   например, lst = [1 , 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 4, 6]
+import random
+
+def get_rand_int_list(low, high, size):
+    return [random.randint(low, high) for i in range(size)]
+
+rand_list = get_rand_int_list(0, 10, 10)
+print(rand_list)
+
+uniq_list = []
+for i in rand_list:
+    if uniq_list.count(i) == 0:
+        uniq_list.append(i)
+
+print(uniq_list)
+
+not_repeated_list = []
+for i in rand_list:
+    if rand_list.count(i) == 1:
+        not_repeated_list.append(i)
+
+print(not_repeated_list)
