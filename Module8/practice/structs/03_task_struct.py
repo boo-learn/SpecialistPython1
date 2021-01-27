5372b5356d0f6e4fce09a169783c8a67bb6ab813
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-27 20:12:38 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,22 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+
+def get_rand_int_list(low, high, size):
+    return [random.randint(low, high) for _ in range(size)]
+
+rand_list = get_rand_int_list(5, 20, 10)
+print(rand_list)
+
+less_ten_list = list(filter(lambda x: x <= 10, rand_list))
+print(less_ten_list)
+
+sum_all = sum(list(filter(lambda x: x > 0, rand_list)))
+print(sum_all)
+
+even_list = list(filter(lambda x: not x % 2, rand_list))
+print(even_list)
+
+avarage = sum(even_list)/len(even_list)
+print(avarage)
