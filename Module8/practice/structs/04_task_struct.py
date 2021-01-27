4ba71060810044e7ef40a10b9f53d5d44edd0966
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-27 20:19:21 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,8 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+
+for el in dictionary_2:    # Наверное не совсем корректно (???)
+    dictionary_1[el] = dictionary_2[el]       
+
+print(dictionary_1)
