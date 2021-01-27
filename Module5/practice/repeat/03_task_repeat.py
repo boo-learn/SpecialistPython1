6e0693d92838c36f8b4796da936eec05c4105094
AlexeyEgorov86 <77969661+AlexeyEgorov86@users.noreply.github.com>
2021-01-27 23:33:28 +0300

    Update 03_task_repeat.py

--- Module5/practice/repeat/03_task_repeat.py
+++ Module5/practice/repeat/03_task_repeat.py
@@ -1,5 +1,7 @@
 # Найдите количество чисел являющихся палиндромами в диапазоне от a до b включительно
 # Известно, что a и b целые положительные числа.
 
-a = ...
-b = ...
+a = 10
+b = 70
+print(list(map(lambda number: str(number) == str(number)[::-1], range(a, b+1))))
+print(sum(map(lambda number: str(number) == str(number)[::-1], range(a, b+1))))
