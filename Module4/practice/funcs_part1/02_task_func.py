eb01fe514407dfa54a5b49d446b0c79703a38048
KirillShiryaev <77969765+KirillShiryaev@users.noreply.github.com>
2021-01-27 11:40:21 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,7 +4,16 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
+    temp_number = number
+    revers_number = 0
+    while (number > 0):
+        digit = number % 10
+        number = number // 10
+        revers_number = revers_number * 10
+        revers_number += digit
+    if (temp_number == revers_number):
+        return "YES"
+    return "NO"
 
 
 # Тестируем функцию
