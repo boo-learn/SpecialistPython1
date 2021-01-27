6b1e8ff4f93748526960cf1eedaab1c187d204e5
DimitryRukhadze <77689849+DimitryRukhadze@users.noreply.github.com>
2021-01-27 22:59:12 +0300

    Update 01_hw.py
    Либо неверно проверяюЮ, либо не понимаю логику решения.

--- Module2/home_work/01_hw.py
+++ Module2/home_work/01_hw.py
@@ -1,8 +1,32 @@
-# Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
-# если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
-# Формат входных данных
 # Вводятся 3 натуральных числа n, m и k. Точно известно, что  k ≠ n ⋅ m.
 # Формат выходных данных
 # Выведите «YES», если можно отломить от шоколадки ровно k долек, и «NO» иначе.
 
 # TODO: your code here
+
+m_chock_size = int(input('Enter m-side: '))
+n_chock_size = int(input('Enter n-side: '))
+k = int(input('How much parts?: '))
+
+break_size = 0
+i = 1
+while (m_chock_size * n_chock_size) % 2 == 0:
+    if m_chock_size > n_chock_size:
+        m_chock_size /= 2
+    else: #возможно, что стороны будут равны, но при этом данное условие тоже работает
+        n_chock_size /= 2
+
+    i *= 2
+
+print (m)
+
+
+if i == k:
+    print('Yes')
+else:
+    print('No')
+
+#if ((i + 1) * 2) == (k * 2):
+#    print('Yes')
+#else:
+#    print('No')
