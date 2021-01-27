cc482706c778f06a0ce86f856797d0af9222cb0d
Artur-dev-sci <77688246+Artur-dev-sci@users.noreply.github.com>
2021-01-27 21:21:34 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,27 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+
+# Это и есть функцяи декоратор
+def new_decorator(function_to_decorate):
+    def wrapper():
+        str = function_to_decorate()  # Сама функция
+        l = len(str)
+        str2 = "**"
+        while l > 0:
+            str2 += '*'
+            l -= 1
+        print(str2)
+        print('*'+str+'*')
+        print(str2)
+    return wrapper
+
+
+# Вот так просто мы можем декорировать любую функцию
+@new_decorator
+def simple_function():
+    return "Hello"
+#
+#
+# # Вызываем декорируемую функцию:
+simple_function()
