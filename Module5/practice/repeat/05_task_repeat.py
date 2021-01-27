0ba2ec224befabd95216331d45180ede3d5d0f8c
Andrej9173 <x9173@yandex.ru>
2021-01-27 23:35:39 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,12 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination(num_items, items_on_page):
+    pages = num_items % items_on_page
+    return pages
+
+
+objects = 1022
+one_page_elements = 10
+
+print(pagination(objects, one_page_elements))
