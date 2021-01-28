2c8a2adc7433b4288bda1c057ff0971b3e5cc536
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-28 11:25:44 +0300

    Update 02_task_decor.py

--- Module8/practice/decorators/02_task_decor.py
+++ Module8/practice/decorators/02_task_decor.py
@@ -1,3 +1,26 @@
 # Напишите функцию декоратор, которая округляет значение декорируемой функции до двух знаков после  запятой.
 # Если округление невозможно, например там строка, или не требуется(целое число) то оставляем результат без изменений.
 # Примечание: используйте встроенную функцию round()
+
+import random
+
+def float_decorator(foo):
+    def wrapper(data):
+        try:
+            result = round(foo(data), 2)
+            return result if len(str(data)) > len(str(result)) else data
+        except ValueError:
+            print('Ошибка преобразования!')
+            return data
+
+    return wrapper
+
+@float_decorator
+def get_float(in_data):
+    return float(in_data)
+
+print(get_float('aerw'))
+print(get_float(42))
+print(get_float('42'))
+print(get_float(42.4242))
+print(get_float('-42.4242'))
