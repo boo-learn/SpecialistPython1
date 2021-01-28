92ade87a37ea57c5226a84a92173b732072a4926
Andrej9173 <x9173@yandex.ru>
2021-01-28 20:31:57 +0300

    Update 02_hw_file.py
    никак не смог придумать, как обработать два минуса.
    Также пробовал line.isdigit(), но тогда не работают отрицательные числа.

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,21 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
+def is_digit(string):
+    if string.isdigit():
+       return True
+    else:
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
+
+total = 0
 with open("data/info.txt", "r") as f:
-    pass
+    for line in f:
+        # if line.strip().isdigit():
+        if is_digit(line.strip()):
+            # print(line)
+            total += float(line.strip())
+print(total)
