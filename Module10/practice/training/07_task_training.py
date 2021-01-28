460f6a94514c6f4c9458889ce121d9c3a3c368b5
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-28 21:48:48 +0300

    Update 07_task_training.py

--- Module10/practice/training/07_task_training.py
+++ Module10/practice/training/07_task_training.py
@@ -1,1 +1,7 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+import random
+
+str_num = [str(random.randint(1, 9))] # Первый элемент не может быть нулём
+str_num.extend([str(random.randint(0, 9)) for _ in range(20000 - 1)]) # Досыпаем остальных в массив
+with open('num_dump.txt', 'w') as file:
+    file.write(''.join(str_num)) # Объеденяем и сохраняем
