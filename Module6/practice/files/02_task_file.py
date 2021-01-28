826332be12dfbc991e8693cda75f1e372bf7304e
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-01-28 12:49:56 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,27 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+
+f = open("data\sold.txt", "r", encoding="utf-8")
+
+sold_sum = 0
+prices = []
+
+for line in f:
+    line = line.split()
+    for num in line:
+        prices.append(float(num))
+
+min_sold_price = prices[0]
+max_sold_price = prices[0]
+
+for price in prices:
+    if price < min_sold_price:
+        min_sold_price = price
+    if price > max_sold_price:
+        max_sold_price = price
+    sold_sum += 1
+
+print("expensive:", max_sold_price)
+print("cheap:", min_sold_price)
+print("total sold:",sold_sum)
