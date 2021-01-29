12562c893bfbf259d737316f11bb9f0ccc395533
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-29 19:45:34 +0300

    Update 02_task_funcfact.py

--- Module10/practice/funcs/02_task_funcfact.py
+++ Module10/practice/funcs/02_task_funcfact.py
@@ -4,4 +4,13 @@
 # 7! = 1*2*3*4*5*6*7
 
 def factorial(n):
-    pass
+    
+    if n == 0:
+        return 0
+     
+    if n > 1:
+        return n * factorial(n -1)
+    else:
+        return 1
+
+print(factorial(4))
