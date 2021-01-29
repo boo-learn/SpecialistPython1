e003f472f93f91543521ca7cc0f78d276e9f5cb6
Andrej9173 <x9173@yandex.ru>
2021-01-29 15:06:03 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,24 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+
+def decorator(function_to_decorate):
+    def wrapper():
+        print('*' * (len(function_to_decorate()) + 2))
+        res = function_to_decorate()
+        print(f'*{res}*')  # Сама функция
+        print('*' * (len(function_to_decorate()) + 2))
+
+    return wrapper
+
+
+@decorator
+def function():
+    # print('asasas')
+    return 'asasas'
+
+
+# # Вызываем декорируемую функцию:
+function()
+
+# new_decorator(simple_function)()
