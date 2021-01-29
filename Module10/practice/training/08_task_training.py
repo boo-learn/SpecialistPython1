f9f83f9a1f22fcd84e08e1bed9acfc12f48eb99a
BadLagger <36419578+BadLagger@users.noreply.github.com>
2021-01-29 12:38:49 +0300

    Update 08_task_training.py

--- Module10/practice/training/08_task_training.py
+++ Module10/practice/training/08_task_training.py
@@ -2,3 +2,24 @@
 # Какие цифры встречаются в числе чаще всего?
 # Если несколько цифр встречаются одинаковое максимальное кол-во раз - найди любые.
 # Является ли данное число(20000-значное) четным?
+
+# Читаем из файла
+with open('num_dump.txt') as file:
+  num_str = file.read()
+
+#print('Длина числа: ', len(num_str))
+
+# Преобразуем в список цифер
+num_list = [int(num) for num in num_str]
+# Делаем словарик: {цифра: количество}
+num_dict = {num: num_list.count(num) for num in range(10)}
+# print(num_dict)
+# Находим максимальное
+result = {k: v for k, v in filter(lambda x: num_dict[x[0]] == max(num_dict.values()), num_dict.items())}
+print(result)
+
+# Определёние чётности
+if num_list[-1] % 2:
+    print('Число не чётное')
+else:
+    print('Число чётное')
