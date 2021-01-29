0e10f81a5b150632500f89fec8e9e3c2b25df418
astaf89 <77969833+astaf89@users.noreply.github.com>
2021-01-29 11:12:51 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,19 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+while True:
+    try:
+        string = input("input <number1>x<number2>: ")
+        #print(string.split("x"))
+        n = float(string.split("x")[0])
+        m = float(string.split("x")[1])
+        # print("n =", n, "\tm =", m)
+        # print(m>n)
+        break
+    except ValueError:
+        print("incorrect values..\n try again")
+
+if m > n:
+    print("0 squares")
+else:
+    print(round(n//m), "squares"
