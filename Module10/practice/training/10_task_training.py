767dd4c979a510b2e88cf2fec16136a1accae7c6
Artur-dev-sci <77688246+Artur-dev-sci@users.noreply.github.com>
2021-01-29 14:35:08 +0300

    Update 10_task_training.py

--- Module10/practice/training/10_task_training.py
+++ Module10/practice/training/10_task_training.py
@@ -2,4 +2,24 @@
 # заполненный случайными целыми числами в диапазоне от a до b.
 # Примечание: все элементы списка должны быть гарантировано уникальными(неповторяющимися)
 # Если создать список с заданными параметрами невозможно - функция должна выбросить ислючение
+import random
 
+
+def gen_list(count, start, stop):
+    data = []
+    try:
+        if (stop - start) < count:
+            raise ValueError
+        while len(data) < count:
+            rand_value = random.randrange(start, stop)
+            if data.count(rand_value) == 0:
+                data.append(rand_value)
+        return data
+    except ValueError:
+        print("Невозможно создать такой список")
+        return None
+
+
+print(gen_list(89, 1, 90))
+
+print(gen_list(90, 1, 90))
