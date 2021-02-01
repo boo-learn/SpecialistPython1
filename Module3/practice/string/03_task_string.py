ea10f96208aa0ebfe1d2fc10ad3faf49519249d9
DimitryRukhadze <77689849+DimitryRukhadze@users.noreply.github.com>
2021-02-01 15:57:04 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,16 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+
+text = 'English is a West Germanic language'
+
+i = 0
+j = 0
+print(len(text))
+
+while i < len(text):
+    if text[i] == 'a':
+        j += 1
+    i += 1
+
+print ('There is ', j, 'letters a in this text')
