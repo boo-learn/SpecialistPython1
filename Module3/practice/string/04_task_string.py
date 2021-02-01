207cffc6739e5f5fb5985ae665b015a27f440097
DimitryRukhadze <77689849+DimitryRukhadze@users.noreply.github.com>
2021-02-01 16:36:56 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -1,7 +1,20 @@
-# Дана строка текста, слова разделены пробелами, знаки препинания отсутствуют.
-# Определить в предоставленном сообщении количество слов длиной больше, чем 5.
-
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+i = 0
+j = 0
+long_word = 0
+
+while i < len(text):
+    if text[i] != ' ':
+        j += 1
+    elif j > 5:
+        long_word += 1
+        if text[i] == ' ':
+            j = 0
+    else:
+        j = 0
+
+    i += 1
+
+print ('There is ', long_word, 'words in this text, that are longer then 5 letters')
