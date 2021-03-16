7e4fcde2d218d75e651e469bc9c3736c2538cd79
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-16 22:00:19 +0300

    Update 04_hw.py

--- Module2/home_work/04_hw.py
+++ Module2/home_work/04_hw.py
@@ -2,7 +2,7 @@
 # Формат входных данных
 # На вход программе одно целое число a (2<a≤30) - сторона квадрата.
 # Формат выходных данных
-# Требуется вывести диагонали символами # (см. пример)
+# Требуется вывести стороны символами # (см. пример)
 
 # Пример:
 # Входные данные
@@ -14,3 +14,33 @@
 #    #
 #    #
 ######
+
+n = int(input("n: "))
+
+# upper horizontal
+i = 1
+str = "#"
+while i < n:
+    str = str + "#"
+    i = i+1
+print(str)
+
+# horizontals
+j = 2
+while j < n: #looper for horizontals
+    str = "#"
+    i = 2
+    while i < n: #looper for backspaces within one line
+        str = str + " "
+        i = i+1
+    str = str + "#"
+    print(str)
+    j = j+1
+
+# lower horizontal
+i = 1
+str = "#"
+while i < n:
+    str = str + "#"
+    i = i+1
+print(str)
