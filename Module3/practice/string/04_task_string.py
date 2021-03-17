a25c4be818f97dd290c13ce41d73fe3ae39a3645
cjingithub <62326549+cjingithub@users.noreply.github.com>
2021-03-17 12:22:11 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,10 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+cnt = 0
+
+for word in text.split():
+    if len(word) > 5:
+        cnt += 1
+
+print(f"cnt: {cnt}")
