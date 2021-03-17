d191ee096f2844d3376a248b7e376f6bdccaa998
papadjaristey <71938222+papadjaristey@users.noreply.github.com>
2021-03-17 12:46:09 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -1,7 +1,15 @@
 # Дана строка текста, слова разделены пробелами, знаки препинания отсутствуют.
 # Определить в предоставленном сообщении количество слов длиной больше, чем 5.
-
-text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+string = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+words = string.split(" ")
+
+
+i = 0
+count = 0
+for word in words:
+    if len(word) > 5:
+        count = count + 1
+
+print(count)
