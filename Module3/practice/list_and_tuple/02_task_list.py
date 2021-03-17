5e8117a4676883015f25030829b69565c6a338f8
skirienkov <80748461+skirienkov@users.noreply.github.com>
2021-03-17 12:58:49 +0300

    Update 02_task_list.py

--- Module3/practice/list_and_tuple/02_task_list.py
+++ Module3/practice/list_and_tuple/02_task_list.py
@@ -2,5 +2,7 @@
 # Каждый элемент должен быть выведен с новой строки.
 
 fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
-
-# TODO: your code here
+i = 0
+for fruit in fruits:
+    i += 1
+    print(i, fruit)
