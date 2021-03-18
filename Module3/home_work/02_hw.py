47fe6db474fd799c4ff08608861b6fbde83ac264
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-18 08:38:46 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -2,7 +2,15 @@
 # в диапазоне от -100 до 100. В списке должно быть n - элементов.
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
+
 import random
 numbers = []
-# print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
-# TODO: your code here
+
+n = int(input("Please enter n: "))
+i = 0
+
+while i < n:
+    numbers.append(random.randint(-100, 100))
+    i = i + 1 
+
+print(numbers)
