82536a42722e934ff51b2237aebcc0cf08603e35
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-18 10:41:28 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -1,4 +1,24 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех положительных элементов.
 
-# TODO: your code here
+import random
+numbers = []
+
+n = int(input("Please enter n: "))
+i = 0
+
+while i < n:
+    numbers.append(random.randint(-10, 10))
+    i = i + 1
+
+print ("List: ", numbers)
+
+i = 0
+sum = 0
+
+while i < n:
+    if numbers[i] > 0:
+        sum = sum + numbers[i]
+    i = i + 1
+    
+print ("Sum of positives: ", sum)
