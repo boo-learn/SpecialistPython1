7fdd174a14e697b750c7623dfb33642c6d5d2606
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-18 11:13:01 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,15 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+new_el = first_number
+my_list = [first_number]
+
+while new_el < second_number:
+    new_el = new_el + 1
+    my_list.append (new_el)
+        
+print ("My list: ", my_list)
+
+for el in my_list:
+    if el % 3 == 0:
+        print (el)
