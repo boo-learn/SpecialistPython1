04a0cd7a24eb6acacb7092dc4d344c623aef0c56
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-19 12:11:37 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -1,10 +1,10 @@
 # Напишите функцию, возвращающую наибольшее из двух чисел
 
 def max2(n1, n2):
-    # TODO: your code here
-    pass
-
-
+    if n1>n2:
+        return n1
+    return n2
+    
 # Тестируем функцию
 print(max2(5, 6))
 print(max2(-10, -12))
