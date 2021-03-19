6569b5e3a7b4bdfe4e0adf34d54f12d38d0731b1
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-19 12:27:06 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,8 +4,15 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
-
+    temp = number
+    number_rev = 0
+    while temp > 0:
+        number_rev = number_rev * 10 + temp % 10 #take last digit in the original number, place it at the beginning of new one
+        temp = temp // 10 #shift the original number to the right
+    if number == number_rev:
+        return 0 #positve answer - number is palindrome
+    else:
+        return -1 #negative answer - number is not palindrome
 
 # Тестируем функцию
 print(palindrome(3454))
