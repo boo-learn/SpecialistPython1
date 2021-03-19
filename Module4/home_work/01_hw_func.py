47592b5cd1d85f07be921a38b768080fa61fdf91
cjingithub <62326549+cjingithub@users.noreply.github.com>
2021-03-19 09:18:19 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -2,12 +2,38 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
+ll = []
+rr = []
+
+
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    ticket_number = str(ticket_number)
+    if len(ticket_number) != 6:
+        print(f"ticket_number:{ticket_number} has not 6 digits")
+        return 1
+
+    left = ticket_number[:-3]
+    for el in left:
+        el = int(el)
+        ll.append(el)
+
+    left_sum = sum(ll)
+
+    right = ticket_number[-3:]
+    for el2 in right:
+        el2 = int(el2)
+        rr.append(el2)
+
+    right_sum = sum(rr)
+
+    if left_sum == right_sum:
+        print(f"ticket number:{ticket_number} is lucky ticket!")
+    else:
+        print(f"ticket number:{ticket_number} is NOT lucky ticket!")
+
+    return ticket_number
 
 
-# Тестируем функцию
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+lucky_ticket(123006)
+lucky_ticket(12321)
+lucky_ticket(436751)
