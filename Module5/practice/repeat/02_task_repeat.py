2f762130e6e27037102436efd81e2772f8f03242
AnnaPavl19 <80748475+AnnaPavl19@users.noreply.github.com>
2021-03-23 10:43:25 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,15 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    n = number
+    revers = 0
+    while n > 0:
+        digit = n % 10
+        n = n // 10
+        revers = revers * 10 + digit
+    if number == revers:
+        return (True)
+    return (False)
+
+print(palindrome(3454))
+print(palindrome(3663))
