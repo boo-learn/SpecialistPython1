a77d6899efc5dddc5ee34e29739c2fed8a5858a3
papadjaristey <71938222+papadjaristey@users.noreply.github.com>
2021-03-24 11:09:29 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -9,9 +9,15 @@ f = open("data/limericks.txt", "r", encoding="utf-8")
 sym_count = 0
 lick_count = 0
 
+
 for line in f:
     if line == "\n":
         lick_count += 1
+    for char in line:
+        if char != " " and char != "\n":
+            sym_count += 1
 
     print(line.rstrip())  # 1.
+print("")
+print("Число символов в файле -", sym_count) # 2.
 print("Число стихотворений -", lick_count)  # 3.
