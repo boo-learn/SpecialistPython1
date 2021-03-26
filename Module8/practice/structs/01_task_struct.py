efa9d66d3bbcae13f09b02eb104282e18d24f098
rakitin-msk <80748441+rakitin-msk@users.noreply.github.com>
2021-03-26 12:11:55 +0300

    Update 01_task_struct.py

--- Module8/practice/structs/01_task_struct.py
+++ Module8/practice/structs/01_task_struct.py
@@ -1,3 +1,19 @@
 # Бегун проводил ежедневные тренировки и записывал расстояния которые пробел за занятия в метрах:
 distances = [600, 755, 321.6, 1234, 231, 456.6, 4561, 1200, 456]
 # Переведите все значения в футы (получить и вывести новый список)
+
+feet_koeff = 3.28084
+
+distances_feets = []
+
+"""
+# using "simple for" cycle:
+
+for el in distances:
+    distances_feets.append(float(float(el)*feet_koeff))
+"""
+
+distances_feets = list(map(lambda el: el * feet_koeff, distances))
+
+print(distances)
+print(distances_feets)
