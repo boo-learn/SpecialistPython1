d120433143384b5fb0647a903ef1f1e5a53e9e4c
Elena <74624648+ElenaKiriakova@users.noreply.github.com>
2021-04-12 16:11:14 +0300

    Update 06_task_if_else.py

--- Module2/practice/if_else/06_task_if_else.py
+++ Module2/practice/if_else/06_task_if_else.py
@@ -8,3 +8,9 @@
 year = int(input("Год: "))
 
 # TODO: your code here
+year = int(input("Год: "))
+
+if (year%4 == 0 and year%100 != 0) or year%400 == 0:
+    print("Високосный")
+else:
+    print("Невисокосный")
