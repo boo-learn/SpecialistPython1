564b01167413ed5bfb9334cf017fa8fd6cc21d94
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-12 15:58:54 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,12 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+
+number = int(input())
+string = ""
+if number % 3 == 0:
+	string = "Foo"
+if number % 5 == 0:
+	string = string + "Bar"
+
+print(string)
