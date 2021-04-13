f189e2749246e92b34c60a007592bf9338e9d259
KoDi0809 <82385284+KoDi0809@users.noreply.github.com>
2021-04-13 16:41:07 +0700

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,10 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+text = input('Строка: ')
+count = 0
+mas_text = text.split(' ')
+for i in range(len(mas_text)):
+    if len(mas_text[i]) > 5:
+        count += 1
+print(f'Количество слов длиной больше 5 символов: {count}')
