672a170939955448c3566c016bbe257a3731abfa
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-13 14:46:42 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+from random import randint
+
+n = int(input("n: "))
+i, my_list = 0, []
+
+while i < n:
+    my_list.append(randint(-10,10))
+    i += 1
+
+total = 0
+for elt in my_list:
+    total += elt if elt > 0 else 0
+
+print(my_list)
+print(total)
