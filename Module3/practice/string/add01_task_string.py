4f58686202ee67303d08cefd96e151e8f45e77bf
KoDi0809 <82385284+KoDi0809@users.noreply.github.com>
2021-04-13 17:13:40 +0700

    Update add01_task_string.py

--- Module3/practice/string/add01_task_string.py
+++ Module3/practice/string/add01_task_string.py
@@ -6,5 +6,13 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat
        "Donec tincidunt sed lorem et feugiat. Nullam elementum"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 # Примечание: обратите внимание на перенос длинной строки на новую строку
+text = input('Строка: ')
+count = 0
+text = text.replace(',', '').replace('.', '').replace('!', '').replace('?', '').replace('?', '-')
+mas_text = text.split(' ')
 
-# TODO: your code here
+for i in range(len(mas_text)):
+    if len(mas_text[i]) > 7:
+        count += 1
+
+print(f'Количество слов длиной больше 7 символов: {count}')
