c636f3352308f98ba3b2adb17b0eb6ea1579b494
KoDi0809 <82385284+KoDi0809@users.noreply.github.com>
2021-04-13 17:04:24 +0700

    Update 05_task_string.py

--- Module3/practice/string/05_task_string.py
+++ Module3/practice/string/05_task_string.py
@@ -3,6 +3,16 @@
 # Строка называется «перевертышем», если читается одинаково слева направо и справа налево.
 
 # Пример строки перевертыша: "И темен город. Мороз узором дорог не мети."
+text = input('Строка: ')
+text = text.replace(' ', '')
+text_replace = text
+for i in range(len(text)):
+    if not text[i].isalpha():
+        text_replace = text.replace(text[i], '')
 
-# TODO: your code here
+text_revers = text_replace[::-1]
 
+if text_revers.lower() == text_replace.lower():
+    print('Перевертыш')
+else:
+    print('Не перевертыш')
