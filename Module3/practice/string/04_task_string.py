a2d13e96bcb8f6ee14032c55c02cd1cfefd02f96
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-13 12:43:33 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,10 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+text_split = text.split()
+count_word = 0
+for word in text_split:
+    if len(word) > 5:
+        count_word += 1
+
+print(count_word)
