a2847adf3e395632767b7f7598ac8b6bf229ab46
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-13 14:50:40 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,13 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input())     # Первое число
+second_number = int(input())    # Второе число
+
+# TODO: your code here
+list_3 = []
+for i in range(first_number + 1, second_number):
+    if i % 3 == 0:
+        list_3.append(i)
+
+print(list_3)
