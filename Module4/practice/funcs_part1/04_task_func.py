d7b5aa151e7b6dfaceb401b3874c6b3cd4a095d4
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-04-14 12:29:08 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -1,11 +1,16 @@
 # Даны координаты трех точек p1(x1; y1) p2(x2; y2) p3(x3; y3).
 # Напишите функцию, проверябщую можно ли построить треугольник, соединив данные точки отрезками
 
-def can_triangle(p1, p2, p3):
-    # TODO: your code here
-    pass
-
-
+def can_triangle (p1,p2,p3):
+        a = ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**0.5
+        b = ((p1[0]-p3[0])**2+(p1[1]-p3[1])**2)**0.5
+        c = ((p3[0]-p2[0])**2+(p3[1]-p2[1])**2)**0.5
+        if a + b > c and b + c> a and a + c > b:
+            perimetr = a + b + c
+            area = (perimetr*(perimetr-a)*(perimetr - b)*(perimetr - c)) ** 0.5
+            return (perimetr, area)
+        else:
+            return ('impossible')
 # Пример вызова функции
 can_triangle((10, 12), (14, 18), (12, 12))
 
