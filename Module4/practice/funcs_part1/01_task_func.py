6a224c3eaaa54f77ba4df69e649d94fd8112af86
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-04-14 12:07:52 +0300

    Update 01_task_func.py

--- Module4/practice/funcs_part1/01_task_func.py
+++ Module4/practice/funcs_part1/01_task_func.py
@@ -1,8 +1,18 @@
 # Напишите функцию, возвращающую наибольшее из двух чисел
 
 def max2(n1, n2):
-    # TODO: your code here
-    pass
+    if n1>n2:
+        result = n1
+    else:
+        result = n2
+    return result
+
+# Тестируем функцию
+print(max2(6, 6))
+print(max2(-10, -12))
+print(max2(2.5, 2.6))
+print(max2(-2.5, 0))
+print(max2(0, -2.5))
 
 
 # Тестируем функцию
