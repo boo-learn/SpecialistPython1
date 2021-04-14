f5504955f4fe68c2c67715f545605187ad3bd2c6
BaggyLaggy <82385057+BaggyLaggy@users.noreply.github.com>
2021-04-14 12:14:23 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -3,8 +3,12 @@
 # Пример палиндрома: 34543
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
+
 def palindrome(number):
-    pass
+    if str(number) == str(number)[::-1]:
+        return str(number)
+
+
 
 
 # Тестируем функцию
