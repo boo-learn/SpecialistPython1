e6c5bc378b023ad0826ac6c548e23f027c608991
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-04-14 23:27:09 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -3,11 +3,16 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    s = str (ticket_number)
+    return True if (int(s[0]) + int(s[1]) + int(s[2]) == int(s[3]) + int(s[4]) + int(s[5])) else False
+
+
+print(lucky_ticket(123006))
+print(lucky_ticket(123210))
+print(lucky_ticket(436751))
 
 
 # Тестируем функцию
 print(lucky_ticket(123006))
-print(lucky_ticket(12321))
+#print(lucky_ticket(12321)) 5-значный номер
 print(lucky_ticket(436751))
