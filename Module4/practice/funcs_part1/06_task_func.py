cfe1f6dd06a78f147ffea488cbf7b27575e246b9
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-14 12:57:19 +0300

    Update 06_task_func.py

--- Module4/practice/funcs_part1/06_task_func.py
+++ Module4/practice/funcs_part1/06_task_func.py
@@ -1,8 +1,12 @@
 # Напишите функцию, которая проверит, что точка (x, y)
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 def point_in_circle(x, y, xc, yc, r):
-    # TODO: your code here
-    pass
+    d = distance(x, y, xc, yc)
+    return d < r
 
 # Не забудьте протестировать вашу функцию
+print(point_in_circle(3, 4, 0, 0, 5))
+print(point_in_circle(3, 4, 0, 0, 5.1))
