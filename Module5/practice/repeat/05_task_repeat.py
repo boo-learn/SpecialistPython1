e07a5ccad25644ebead4f3fda1929b18b728caf0
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-04-14 15:43:33 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,7 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination_last_page (num_items, items_on_page):
+    return (num_items % items_on_page)
+
+print('Pages:', pagination_last_page (111,25))
