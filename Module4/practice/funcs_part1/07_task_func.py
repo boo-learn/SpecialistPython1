f27fd5a7c3c0f97466bf6db0fa5da3a9a3779fc9
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-14 13:09:48 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,9 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def str_time(sec_total):
+    m_total = sec_total // 60
+    ss = sec_total - m_total * 60
+    hh = m_total // 60
+    mm = m_total - hh * 60
+    return f"{hh:0>2}:{mm:0>2}:{ss:0>2}"
