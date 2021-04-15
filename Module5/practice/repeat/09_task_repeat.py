11a7f4590475d19e46c96ee44df2ff8ac8fee22f
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2021-04-15 17:35:48 +0300

    Update 09_task_repeat.py

--- Module5/practice/repeat/09_task_repeat.py
+++ Module5/practice/repeat/09_task_repeat.py
@@ -6,3 +6,30 @@
 # Результат: фруктов на букву “к” больше.
 # Дано: [“ананас”, “яблоко”, “Арбуз”, “киви”, “Клюква”, “банан”, “хурма”]
 # Результат: фруктов на букву “к”и “а” больше.
+
+fruits= ["ананас", "Баклажан", "Арбуз", "киви", "Клюква", "банан", "хурма"]
+
+first_letters_tup=tuple(map(lambda el: el[0].lower(),fruits))
+# first_letters_set=set(map(lambda el: el[0].lower(),fruits))
+first_letters_set=set(first_letters_tup)
+
+# Вариант 1 отбора
+first_letters_dict={}
+for el in first_letters_set:
+    first_letters_dict[el]=first_letters_tup.count(el)
+
+# Вариант 2 отбора
+first_letters_dict1={el:first_letters_tup.count(el) for el in first_letters_set}
+
+max_count=max(first_letters_dict.values())
+
+# Вариант 1 вывода
+max_list=[]
+for i in first_letters_dict.items():
+    if i[1]==max_count:
+        max_list.append(i)
+print(f"Буквы, встречающиеся чаще всего без учета регистра {max_list}")
+
+# Вариант 2 вывода
+max_list1=list(filter(lambda i:i[1]==max_count,first_letters_dict1.items()))
+print(f"Буквы, встречающиеся чаще всего без учета регистра {max_list1}")
