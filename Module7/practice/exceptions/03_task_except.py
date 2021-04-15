09ad6be1d49e23c9c7c3757aeb21bd2c532e6f7f
ElisKik <82385107+ElisKik@users.noreply.github.com>
2021-04-15 15:43:24 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,21 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+def run():
+    numbers = get_numbers()
+    min_positive = get_min_positive(numbers)
+    print(min_positive)
+
+def get_numbers():
+    try:
+        input_str = input('Введите несколько чисел, разделяя пробелом (пример: 1 2 3 4 -7 8 5): ')
+        return [int(num_str) for num_str in input_str.split(' ')]
+    except Exception as error:
+        print(f'Ошибка во время разбора входных данных: {error}')
+        return get_numbers()
+
+def get_min_positive(numbers):
+    return min(filter(lambda num: num > 0, numbers))
+
+run()
