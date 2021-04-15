ecfee5e21adfc08be4b5c25001ddf872876e77be
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-15 11:51:40 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,25 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+f = open('data/sold.txt', 'r', encoding='utf-8')
+
+
+sold_items = []
+for line in f.readlines():
+    line_x = line.rstrip().split(' ')
+    for item in line_x:
+        sold_items.append(float(item))
+total = 0
+min_sold = sold_items[0]
+max_sold = 0
+for item in sold_items:
+    total += item
+    if item < min_sold:
+        min_sold = item
+    if item > max_sold:
+        max_sold = item
+
+print(sold_items)
+print(f'Сумма товаров = {total}, макс.стоимость = {max_sold}, мин.стоимость = {min_sold}')
+
+f.close()
