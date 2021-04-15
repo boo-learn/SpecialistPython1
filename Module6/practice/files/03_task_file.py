b9b3c4d372d4f9da4f4eb80f35c6dc257fb38828
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-15 12:45:42 +0300

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -6,3 +6,27 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+f = open('data/items_sold.txt', 'r', encoding='utf-8')
+
+items_sold = {}
+for line in f:
+    for item in line.strip().split():
+        val = item.split(':')
+        if items_sold.get(val[0]) is None:
+            items_sold[val[0]] = float(val[1])
+        else:
+            items_sold[val[0]] += float(val[1])
+
+print(items_sold)
+
+max_sold = max(items_sold.values())
+key_max_sold = ''
+for key, item in items_sold.items():
+    if item == max_sold:
+        key_max_sold = key
+
+print(f'Товар на самую большую сумму: {key_max_sold}')
+
+print(f'Кол-во типов товаров: {len(items_sold.keys())}')
+
+f.close()
