5ab5a1bd3de9e223f1da12c3d9d836826cc5dad5
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-15 19:16:25 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,14 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    import re
+
+pattern = re.compile('[-]{0,1}[0-9]{1,}')
+with open("data/info.txt", "r") as f:
+    items = []
+    for line in f:
+        if pattern.findall(line.rstrip()):
+            items.append(int(pattern.findall(line.rstrip())[0]))
+
+print(items)
+print(sum(items))
