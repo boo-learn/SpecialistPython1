ce72bc4b47fd07051154fc630aad7b6542fcceb9
ElisKik <82385107+ElisKik@users.noreply.github.com>
2021-04-15 22:30:00 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,8 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    # Строчки читаются из файла со знаком переноса строки.
+    # Поэтому, в генераторе, перед парсингом int(), очищаем каждую с помощью метода strip()
+    numbers = [int(clean_line) for clean_line in [raw_line.strip() for raw_line in f.readlines()] if clean_line.isdigit()]
+    total = sum(numbers)
+    print(total)
