6ed4d261ef0b85f1f3f5a737a0c20d844c8b9c3f
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-15 12:17:27 +0300

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -6,3 +6,22 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+f_data = open("data/items_sold.txt", "r", encoding="utf-8")
+dic_sales = {}
+for line in f_data:
+    sales = line.split(sep=" ")
+    for sale in sales:
+        pair = sale.split(sep=":")
+        if pair[0] in dic_sales:
+            dic_sales[pair[0]] += float(pair[1])
+        else:
+            dic_sales[pair[0]] = float(pair[1])
+
+print("total: ", sum(dic_sales.values()))
+
+print("sum by item: ", dic_sales)
+
+max_sum = sorted(dic_sales.values(), key=lambda x: -x)[0]
+print("items of max sum: ", list( filter(lambda kv: kv[1] == max_sum, dic_sales.items()) ) )
+
+print("item count: ", len(dic_sales))
