8d5170479395d923402c20b71b4148e4950a1971
Рыжкова Алиса <82371415+RedAliceXXIII@users.noreply.github.com>
2021-04-15 16:50:06 +0300

    Update 02_task_except.py

--- Module7/practice/exceptions/02_task_except.py
+++ Module7/practice/exceptions/02_task_except.py
@@ -4,22 +4,38 @@
 # Алгоритм проверки на високосный год оформите в виде отдельной функции.
 #
 # Входная строка содержит два целых числа – номер месяца (возможно, неправильный) и номер года.
+def days_in_year(num_year):
+    if num_year % 4 == 0 and num_year % 100 != 0 or num_year % 400 == 0:
+        return True
+    return False
+
 days_in_month = (31, 28, 3, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 while True:
-    month = int(input())
     try:
         month = float(input('введите месяц'))
-        year = float(input('введите год'))
-        if float(month) % 1 != 0 or float(year) % 1 != 0:
+        if float(month) % 1 != 0:
             raise ValueError
         month = int(month)
-        year = int(year)
         if month < 1 or month > 12 or :
             raise ValueError
+        break
+    except ValueError:
+        print('некорректные данные')
+        
+while True:
+    try:
+        year = float(input('введите год'))
+        if float(year) % 1 != 0:
+            raise ValueError
+        year = int(year)
         break
     except ValueError:
         print('некорректные данные')
 
 
 days = days_in_month[month - 1]
-# далее коррекция февраля для високосного года
+if month == 2:
+    if days_in_year(year):
+        days +=1
+        
+print(days)
