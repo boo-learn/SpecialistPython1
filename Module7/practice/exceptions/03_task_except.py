6149cb119b3472ed9a64b7de4f2e6d9a57acc746
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-15 17:06:07 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,23 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+import sys
+
+while True:
+    str_input = input("строка из пяти целых чисел, разделенных пробелом: ")
+    str_numbers = str_input.split()
+    try:
+        numbers = [int(x) for x in str_numbers]
+        if len(numbers) != 5:
+            raise ValueError
+        break
+    except (ValueError, TypeError):
+        print("неверный ввод")
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+try:
+    print( min([x for x in numbers if x > 0]) )
+except ValueError:
+    print("Нет подходящих чисел")
