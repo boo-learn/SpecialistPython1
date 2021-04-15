0e56d654c910eccb31d91b3c86f130d354845376
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-15 11:38:02 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,12 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+f_lim_read = open("data/limericks.txt", "r", encoding="utf-8")
+symbs = 0
+lims = 0
+for line in f_lim_read:
+    print(line.rstrip())
+    symbs += len(line.replace(" ","").replace("\n", "").replace("\t",""))
+    if line.strip() == "": lims += 1
+print(symbs, lims+1)
+f_lim_read.close()
