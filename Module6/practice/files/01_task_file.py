2c4ccde753a917f4cb6c24835a23ae2449d99327
BaggyLaggy <82385057+BaggyLaggy@users.noreply.github.com>
2021-04-15 12:00:12 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,28 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+
+f = open("data/limerics.txt", "r", encoding="UTF-8")
+
+
+for line in f:
+    print(line.rstrip())
+
+f.seek(0)
+total = 0
+for line in f:
+    total += len(line.strip().replace(' ', ''))
+
+print(f'Кол-во не пробельных символов {total}')
+
+f.seek(0)
+total = 1
+for line in f:
+    if line == '\n':
+        total += 1
+print(f'Кол-во стихотворений {total}')
+
+
+
+
+
