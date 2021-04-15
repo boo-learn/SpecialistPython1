14e989fb75cfe5aa4a2b879489c5b1b4ec5df9e2
KoDi0809 <82385284+KoDi0809@users.noreply.github.com>
2021-04-15 15:35:30 +0700

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,14 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+f = open('limericks.txt', 'r', encoding='utf-8')
+simbol = 0
+verse = 0
+for line in f:
+    print(line.rstrip())
+    if line.isspace():
+        verse += 1
+    else:
+        simbol += len(line.strip().replace(' ', ''))
+print('Стихов: ', verse + 1)
+print('Не пробельных символов:', simbol)
