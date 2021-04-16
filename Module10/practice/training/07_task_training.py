b130b3f729b5c4526c599612ac89cb40eec1681f
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-16 15:27:00 +0300

    Update 07_task_training.py

--- Module10/practice/training/07_task_training.py
+++ Module10/practice/training/07_task_training.py
@@ -1,1 +1,12 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+import random
+
+seq = []
+digits = "0123456789"
+
+for _ in range(20000):
+    seq.append(digits[random.randint(0,9)])
+
+with open("huge_number.txt", "w") as f:
+    f.write(''.join(seq))
+
