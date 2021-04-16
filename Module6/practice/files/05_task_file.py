7e2f773771a282eb850821d0c6a7115438141f48
AndreevDmitriy <76036672+AndreevDmitriy@users.noreply.github.com>
2021-04-16 14:44:37 +0300

    Update 05_task_file.py
    Изначально хотел реализовать через списки словарей, но запутался немножко. Также не нравится вложенный цикл для нахождения соответствий работников, но не придумывается как реализовать иначе

--- Module6/practice/files/05_task_file.py
+++ Module6/practice/files/05_task_file.py
@@ -3,3 +3,38 @@
 # если отработают норму часов. Если же они отработали меньше нормы, то их ЗП уменьшается пропорционально,
 # а за каждый час переработки они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+import os
+from settings import BASE_DIR
+from pprint import pprint
+
+
+def list_from_file(f):
+    lst = list(line.rstrip() for line in f)
+    f.close()
+    return list(line.split() for line in lst[:])
+
+
+def str_to_digit(lst):
+    for el in lst:
+        i = 0
+        while i < len(el):
+            el[i] = float(el[i]) if el[i].isdigit() else el[i]
+            i += 1
+    return lst
+
+
+file_path_workers = os.path.join(BASE_DIR, "Files", "workers.txt")
+file_path_hours = os.path.join(BASE_DIR, "Files", "hours_of.txt")
+f_workers = open(file_path_workers, "r", encoding="utf-8")
+f_hours = open(file_path_hours, "r", encoding="utf-8")
+list_workers = str_to_digit(list_from_file(f_workers)[1:])
+list_hours = str_to_digit(list_from_file(f_hours)[1:])
+
+# Решение не очень нравится... быстродействие порядка O(n^2). Надо как-то по другому
+for el in list_workers:
+    for i in list_hours:
+        if i[0] + i[1] == el[0] + el[1]:
+            el.append(i[2])
+    el.append(el[2] * el[5] / el[4] if el[5] <= el[4] else el[2] * (1 + 2 * (el[5] - el[4]) / el[4]))
+    el[-1] = round(el[-1], 2)
+pprint(list_workers)
