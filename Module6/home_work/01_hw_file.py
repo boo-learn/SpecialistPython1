bc0a3084d3443f7eb93c99a6e313c1a20455886a
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-16 09:49:06 +0300

    Update 01_hw_file.py

--- Module6/home_work/01_hw_file.py
+++ Module6/home_work/01_hw_file.py
@@ -1,7 +1,17 @@
 # Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
+from pathlib import Path
 
 def log(text, file="log.txt"):
-    pass
+    logged_string = str(text)
+
+    if logged_string.strip() == "":
+        return
+
+    log_file = Path(file)
+    mode = "a" if log_file.is_file() else "w"
+
+    with open(file, mode, encoding="utf-8") as f:
+        f.write(logged_string + "\n")
 
 
 log("hello world")  # дописывает "hello world" в конец файла log.txt
