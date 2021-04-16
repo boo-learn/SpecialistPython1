90678e0f31f8a96fadb6c305635c9d9cc0f6cf9a
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-16 09:52:56 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,15 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+def try_parse_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return 0
+
+total = 0
+try:
+    with open("data/info.txt", "r") as f:
+        for line in f: total += try_parse_int(line.strip())
+except Exception as e:
+    print(e)
