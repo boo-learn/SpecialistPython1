95457baf681d1081506adaf5e1a692772af379e2
IgorZhizhin-1 <82408689+IgorZhizhin-1@users.noreply.github.com>
2021-04-16 12:05:22 +0300

    Update 05_task_struct.py

--- Module8/practice/structs/05_task_struct.py
+++ Module8/practice/structs/05_task_struct.py
@@ -5,3 +5,7 @@
 # Пример: для строки 'pythonist'
 # Получим словарь: {'p': 1, 'y': 1, 't': 2, 'h': 1, 'o': 1, 'n': 1, 's': 1, 'i': 1}
 # Примечание: т.к. ключи неупорядочены, порядок следования ключей может быть произвольным
+src_string = 'pythonist'
+
+letters = {char: src_string.count(char) for char in src_string}
+print(letters)
