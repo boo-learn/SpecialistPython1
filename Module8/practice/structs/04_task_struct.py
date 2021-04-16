510a332d6250dd7c78e8df1a648e903040cf6d7a
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-04-16 12:57:34 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,7 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+for key, value in dictionary_2.items():
+    s = (key,value)
+    dictionary_1.update([s])
+print(dictionary_1)
