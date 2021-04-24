75db64e0b6523a97c70ebc387ad8a667e35d6764
SergeiKharitonov <83123682+SergeiKharitonov@users.noreply.github.com>
2021-04-24 16:22:33 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -1,5 +1,11 @@
 # Дан треугольник со сторонами a, b и c. Определите, является ли он равнобедренным.
 # Формат входных данных: дано три натуральных числа. Гарантируется, что отрезки с данными длинами образуют треугольник.
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
+a = int(input("Please input first side of a triangle: "))
+b = int(input("Please input second side of the triangle: "))
+c = int(input("Please input third side of the triangle: "))
 
-# TODO: your code here
+if a==b or b==c or a==c:
+    print("YES")
+else:
+    print ("NO")
