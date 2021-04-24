ed7893469a5d67b7f0424331343c79cbce68e827
psihu4ka <83123692+psihu4ka@users.noreply.github.com>
2021-04-24 16:14:06 +0300

    Update 02_task_if_else.py

--- Module2/practice/if_else/02_task_if_else.py
+++ Module2/practice/if_else/02_task_if_else.py
@@ -3,3 +3,9 @@
 # Формат выходных данных: Если число оканчивается цифрой 5, вывести «YES», в противном случае — вывести «NO».
 
 # TODO: your code here
+number = int(input())  
+a = number%10
+if a==5:
+    print("yes")
+else:
+    print("no")
