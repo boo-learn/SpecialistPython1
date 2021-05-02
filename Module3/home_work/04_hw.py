6989e2db59b9cd14acbf49736c43710a4d53403b
EsleinSlein <83123675+EsleinSlein@users.noreply.github.com>
2021-05-02 23:18:11 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,14 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import random
+numbers = []
+n = int(input("Введите  n:"))
+for i in range(0, n):
+    numbers.append(random.randint(-100,100))
+print("numbers:", numbers)
+new = []
+for sqrt in numbers:
+    if sqrt > 0 and (sqrt ** 0.5).is_integer():
+        new.append(int(sqrt ** 0.5))
+print("new:", new)
