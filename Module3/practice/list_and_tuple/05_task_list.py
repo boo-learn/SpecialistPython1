e0dd48efaa97953d8b0699371f52e17d1fd071b9
alexs2011 <kir-al93@yandex.ru>
2021-05-02 20:25:17 +0700

    Update 05_task_list.py

--- Module3/practice/list_and_tuple/05_task_list.py
+++ Module3/practice/list_and_tuple/05_task_list.py
@@ -5,7 +5,19 @@
 # Исходные данные:
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
-# TODO: your code here
+max_len = 0
+for fruit in fruits:
+    if len(fruit) > max_len:
+        max_len = len(fruit)
+
+for i, fruit in enumerate(fruits, 1):
+    number_of_spaces = max_len - len(fruit) - len(str(i)) + 1
+    spaces = " "
+    j = 0
+    while j < number_of_spaces:
+        spaces += " "
+        j += 1
+    print(str(i) + "." + spaces + fruit)
 
 # Пример вывода:
 # 1. яблоко
