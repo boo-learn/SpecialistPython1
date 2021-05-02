5a039567a79a7291aa247eabe1d43d945d1a8eaa
Irina0703 <83123781+Irina0703@users.noreply.github.com>
2021-05-02 15:59:30 +0300

    Update 01_task_list.py

--- Module3/practice/list_and_tuple/01_task_list.py
+++ Module3/practice/list_and_tuple/01_task_list.py
@@ -4,3 +4,9 @@
 my_list = [24, "Васлий", "выхухоль", 42, -12.5]
 
 # TODO: your code here
+
+my_list = [24, "Васлий", "выхухоль", 42, -12.5]
+i = 0
+while i < len(my_list):
+    print(my_list [i])
+    i += 1
