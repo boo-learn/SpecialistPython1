a94fa254ebf88c2a6b16c121577fcdca968d77ec
alexs2011 <kir-al93@yandex.ru>
2021-05-03 19:08:47 +0700

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,39 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+def can_triangle(p1, p2, p3):
+    d1 = distance(*p1, *p2)
+    d2 = distance(*p1, *p3)
+    d3 = distance(*p2, *p3)
+    return d1 + d2 > d3 and d1 + d3 > d2 and d2 + d3 > d1
 
 
-# Не забудьте протестировать вашу функцию
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def perimeter(p1, p2, p3):
+    d1 = distance(*p1, *p2)
+    d2 = distance(*p1, *p3)
+    d3 = distance(*p2, *p3)
+    return d1 + d2 + d3
+
+
+def square(p1, p2, p3):
+    a = distance(*p1, *p2)
+    b = distance(*p1, *p3)
+    c = distance(*p2, *p3)
+    p = perimeter(p1, p2, p3) / 2
+
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+points = ((10, 12), (14, 18), (12, 12))
+
+if can_triangle(*points):
+    print(perimeter(*points))
+    print(square(*points))
+else:
+    print("Построить нельзя")
+
+    # Не забудьте протестировать вашу функцию
