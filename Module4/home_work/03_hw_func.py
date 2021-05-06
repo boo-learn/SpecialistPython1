e8371813eb98796fd31a4e4f33409b842ccc2208
alexs2011 <kir-al93@yandex.ru>
2021-05-06 17:41:30 +0700

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -1,4 +1,17 @@
 # Даны координаты центров двух окружностей (x1; y1) (x2; y2) и и их радиусы  R1 и R2.
 # Находится ли одна окружность целиком внутри другой
 
-# TODO: your code here
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def is_circumference_inside(c1, c2, r1, r2):
+    return r1 + distance(*c1, *c2) < r2
+
+
+c1 = 0, 0
+c2 = 1, 1
+r1 = 2
+r2 = 3
+
+print(is_circumference_inside(c1, c2, r1, r2))
