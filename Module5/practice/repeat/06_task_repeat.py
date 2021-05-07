3bc84b138137ac16dd1b482c942fba1c13027134
ElenLit <83123756+ElenLit@users.noreply.github.com>
2021-05-07 23:52:28 +0300

    Update 06_task_repeat.py

--- Module5/practice/repeat/06_task_repeat.py
+++ Module5/practice/repeat/06_task_repeat.py
@@ -6,5 +6,11 @@
 # которые не делятся нацело на 400, високосными не являются.
 
 def days_in_year(num_year):
-    pass
+    return (num_year % 4 == 0 and num_year % 100 != 0) or num_year % 400 == 0
 
+
+year = 2020
+if days_in_year(year):
+    print(f'В году {year} -  366 дней.')
+else:
+    print(f'В году {year} -  365 дней.')
