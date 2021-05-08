e5700462d88a5bbb56f67cb9e1a447eb31b970d5
ElenLit <83123756+ElenLit@users.noreply.github.com>
2021-05-08 14:20:38 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,19 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+f = open('sold.txt', 'r', encoding='utf-8' )
+sum = 0
+max_price = 0
+for line in f:
+    lst = line.split(' ')
+    if sum == 0:
+        min_price = float(lst[0])
+    for price in lst:
+        price = float(price)
+        sum += price
+        if price > max_price:
+            max_price = price
+        if price < min_price:
+            min_price = price
+f.close()
+print(f'all_sum {sum}, max_price {max_price}, min_price {min_price}')
