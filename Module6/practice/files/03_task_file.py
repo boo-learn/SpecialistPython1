85d2b6d1db85269556b1324abec1fc5eef1e7fa3
alexs2011 <kir-al93@yandex.ru>
2021-05-08 18:33:03 +0700

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -6,3 +6,43 @@
 # 2. Какова выручка магазина по каждому типу товаров
 # 3. Какой тип товара был продан на самую большую сумму
 # 4. Сколько различных типов товаров было продано за день
+
+f = open("items_sold.txt", "r", encoding="utf-8")
+
+d = {}
+for line in f:
+    items = line.split(" ")
+    for item in items:
+        tokens = item.split(":")
+        if d.get(tokens[0]) is None:
+            d[tokens[0]] = []
+        val = d[tokens[0]]
+        val.append(float(tokens[1]))
+        d[tokens[0]] = val
+
+f.close()
+
+s = 0
+for _, val in d.items():
+    s += sum(val)
+print(s)
+
+print()
+
+items_val = []
+for key, val in d.items():
+    s = sum(val)
+    items_val.append((key, s))
+    print(f"{key}: {s:.2f}")
+
+print()
+
+max_it = ["", 0]
+for el in items_val:
+    if el[1] > max_it[1]:
+        max_it = el
+print(max_it[0])
+
+print()
+
+print(len(d.keys()))
